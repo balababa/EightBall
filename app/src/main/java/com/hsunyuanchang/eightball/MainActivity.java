225b6021823e314961f91339fa3c3010de711f67
balababa <cool8676@gmail.com>
2017-11-30 00:37:38 +0800

    Initial commit

---
+++ app/src/main/java/com/hsunyuanchang/eightball/MainActivity.java
@@ -0,0 +1,37 @@
+package com.hsunyuanchang.eightball;
+
+import android.media.Image;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final ImageView answer = (ImageView) findViewById(R.id.image_answer);
+        final int[] answerArray = new int[]{
+                R.drawable.ball1,
+                R.drawable.ball2,
+                R.drawable.ball3,
+                R.drawable.ball4,
+                R.drawable.ball5
+        };
+
+        Button button = (Button) findViewById(R.id.rollButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Random rand = new Random();
+                answer.setImageResource(answerArray[rand.nextInt(5)]);
+            }
+        });
+    }
+}
